3522f32497a8c938e163875d38a85a759dc2a304
Zafuzi <zacharyfoutz@gmail.com>
2022-02-08 10:16:20 -0800

    * added proper scaling of canvas

--- game.js
+++ game.js
@@ -1,26 +1,52 @@
-let screen = vec(800, 600);
+let screen = vec(1920, 1080);
 canvas.width = screen.x;
 canvas.height = screen.y;
 
+let ctx;
+
 document.addEventListener("DOMContentLoaded", function()
 {
+
+	ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = true;
+	ctx.imageSmoothingQuality = "high";
+
+	canvas.style.width = "unset";
+	canvas.style.height = "unset";
+	canvas.style.top = "unset";
+	canvas.style.left = "unset";
 
 	let toggleDebug = newInputType("toggleDebug", ["`"]);
 
 	let brain = new Thing();
 		brain.listen("tick", function()
 		{
-			// determine which is smaller
-			if(window.innerWidth > window.innerHeight)
-			{
-				canvas.style.height = window.innerHeight + "px";
-				canvas.style.width = 4 * window.innerHeight / 3 + "px";
-			}
-			else
-			{
-				canvas.style.width = window.innerWidth + "px";
-				canvas.style.height = 3 * window.innerWidth / 4 + "px";
-			}
+			let image = {
+				x: 0, y: 0, width: screen.x, height: screen.y
+			};
+
+			let originalWidth = image.width;
+			let originalHeight = image.height;
+
+			let options = {
+				container: new AS.Size(window.innerWidth, window.innerHeight),
+				target: new AS.Size(originalWidth, originalHeight),
+				policy: AS.POLICY.ShowAll
+			};
+
+			let rect = AS.getScaledRect(options);
+
+			image.x = rect.x;
+			image.y = rect.y;
+			image.width = rect.width;
+			image.height = rect.height;
+
+			console.log(image);
+
+			canvas.style.width = image.width + "px";
+			canvas.style.height = image.height + "px";
+			canvas.style.top = image.top + "px";
+			canvas.style.left = image.left + "px";
 
 			if(toggleDebug.pressed && input.debounce === 0)
 			{
--- nodes/slime.js
+++ nodes/slime.js
@@ -1,11 +1,11 @@
 function slime()
 {
-	load_image("data/slime1.png", function(slime_img)
+	load_image("data/player.png", function(slime_img)
 	{
 		let sq = new Squid(vec(100, 100), slime_img);
 		sq.listen("tick", function()
 		{
-			sq.position.x += 1;
+			//sq.position.x += 1;
 		});
 	});
 }
